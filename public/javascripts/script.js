eed880bad7669e3213f5bf0ca8b351aed41ace7d
Alex Sell <alexanderjsell@gmail.com>
2020-03-04 12:15:54 -0800

    Add express route that makes GET request to SCPR API

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,19 @@
 function onSubmit(event) {
+  // ui logic
   event.preventDefault();
   const queryInput = document.getElementById("query-input");
   let queryValue = queryInput.value;
   console.log('submit:', queryValue);
   queryInput.value = '';
+
+  //xhr logic
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", reqListener);
+  oReq.open("GET", "/api?" + 'search=' + queryValue, true);
+  oReq.send();
+}
+
+function reqListener() {
+  const response = JSON.parse(this.responseText)
+  console.log(response);
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios')
+const SCPR_ARTICLES_URL = 'http://www.scpr.org/api/v3/articles';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/api', function(req, res) {
+  var query = req.query.search
+  axios.get(`${SCPR_ARTICLES_URL}?query=${query}`)
+    .then(function(response) {
+      res.send({data: response.data, query: query})
+    })
+    .catch(function(err) {
+      res.send(err)
+    })
+})
 
 module.exports = router;

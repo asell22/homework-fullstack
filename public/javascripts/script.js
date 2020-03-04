5a6207ce016780aaf58a205ab6fb574d2181c5f5
Alex Sell <alexanderjsell@gmail.com>
2020-03-04 13:03:25 -0800

    Render list of articles with response data

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -16,4 +16,34 @@ function onSubmit(event) {
 function reqListener() {
   const response = JSON.parse(this.responseText)
   console.log(response);
+  const statusCode = response.data.meta.status.code
+  const statusMessage = response.data.meta.status.message
+
+  if ( statusCode === 200 && statusMessage === 'OK' ) {
+    buildArticlesList(response)
+  }
+}
+
+function buildArticlesList(response) {
+  const articles = response.data.articles
+  const articlesContainer = document.getElementById("articles-container")
+  const articlesList = document.createElement('ol')
+  articlesList.id = 'articles-list'
+
+  // build ordered list of articles
+  for(var index in articles) {
+    let article = articles[index];
+    let articleLi = document.createElement('li')
+    articleLi.id = article.id
+    articleLi.innerText = article.title
+    articlesList.appendChild(articleLi)
+  }
+
+  // build header
+  const resultsHeader = document.createElement('h3')
+  resultsHeader.innerText = 'Showing results for ' + response.query + ':'
+
+  //append header and articles
+  articlesContainer.append(resultsHeader)
+  articlesContainer.append(articlesList);
 }
